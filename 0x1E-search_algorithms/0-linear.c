72f92f71fb203c6838cfb29cf84d2b816224ab4b
abenixz <abezelalem.0703@gmail.com>
2022-01-20 01:50:14 +0300

    2 Tasks Edited

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,26 +1,30 @@
 #include "search_algos.h"
 
 /**
-  * linear_search - linear search 
-  * @array: pointer to first element in search array
-  * @size: size of array
-  * @value: value to find
+  * linear_search - Searches for a value in an array
+  *                 of integers using linear search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
   *
-  * Return: index where found or -1
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the first index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
   */
-
 int linear_search(int *array, size_t size, int value)
 {
-	    unsigned int i;
+	size_t i;
 
-	    if (array == NULL)
-		        return (-1);
-	    for (i = 0; i < size; i++)
-	    {
+	if (array == NULL)
+		return (-1);
 
-                printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		        if (array[i] == value)
-			            return (i);
-	    }
-	    return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,14 +6,12 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
  *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
@@ -31,14 +29,13 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
- * for Holberton project
  */
 typedef struct skiplist_s
 {
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
@@ -49,6 +46,6 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-void print_array(int *array, int left, int right);
 
-#endif /* SEARCH_ALGOS_H */
+
+#endif /* SEARCH_ALGO_H */
