4985c3f71b864bf1328c6b85bec485182022c33a
abenixz <abezelalem.0703@gmail.com>
2022-01-20 03:46:57 +0300

    O Linear Added

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
-  * linear_search - linear search 
+  * linear_search - linear search
   * @array: pointer to first element in search array
   * @size: size of array
   * @value: value to find
@@ -11,16 +11,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	    unsigned int i;
+	unsigned int i;
 
-	    if (array == NULL)
-		        return (-1);
-	    for (i = 0; i < size; i++)
-	    {
-
-                printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		        if (array[i] == value)
-			            return (i);
-	    }
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
 	return (-1);
 }
